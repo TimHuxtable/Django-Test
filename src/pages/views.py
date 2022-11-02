c97efa88e1fafecea9abe23e99e9215b2c706452
Timothy Huxtable <huxtable.tim30@gmail.com>
2022-11-02 16:24:52 -0500

    First commit

---
+++ src/pages/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_view(request, *args, **kwargs): # *args, **kwargs
+	# return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+	return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+	return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+	my_context = {
+		"my_text": "This is about us",
+		"my_number": 123
+	}
+	return render(request, "about.html", my_context)
